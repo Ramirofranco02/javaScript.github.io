18f85d7c3de7ab798460bffed17ad66aeda09a25
Ramiro franco <francoramiro175@gmail.com>
2023-10-12 19:32:18 -0300

    generador de pelis

--- js/script.js
+++ js/script.js
@@ -1,20 +1,62 @@
 
-document.getElementById("mostrarButton").addEventListener("click", function() {
-    let numeroIngresado = parseInt(document.getElementById("numeroInput").value);
-    let resultado = "";
+// Definir una lista de películas por género
+const peliculas = {
+    "Acción": ["Mad Max: Fury Road", "John Wick", "Die Hard"],
+    "Comedia": ["Superbad", "The Hangover", "Anchorman"],
+    "Drama": ["The Shawshank Redemption", "Forrest Gump", "The Godfather"],
+    "Ciencia Ficción": ["Blade Runner", "The Matrix", "Inception"],
+    "Aventura": ["Indiana Jones and the Last Crusade", "Pirates of the Caribbean", "Jurassic Park"]
+};
 
-    if (isNaN(numeroIngresado)) {
-        resultado = "Por favor, ingrese un número válido.";
+// Obtener elementos del DOM
+const genreSelect = document.getElementById("genre");
+const searchButton = document.getElementById("searchButton");
+const resultDiv = document.getElementById("result");
+
+// Función para buscar películas por género con animación de desvanecimiento
+function buscarPeliculas() {
+    const selectedGenre = genreSelect.value;
+    const peliculasEnEseGenero = peliculas[selectedGenre];
+
+    if (peliculasEnEseGenero) {
+        let resultado = "<h2>Películas en " + selectedGenre + ":</h2>";
+
+        // Copia y mezcla el array de películas al azar
+        const peliculasAleatorias = [...peliculasEnEseGenero];
+        for (let i = peliculasAleatorias.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [peliculasAleatorias[i], peliculasAleatorias[j]] = [peliculasAleatorias[j], peliculasAleatorias[i]];
+        }
+
+        resultado += "<ul>";
+
+        peliculasAleatorias.forEach((pelicula) => {
+            resultado += "<li style='display: none;'>" + pelicula + "</li>";
+        });
+
+        resultado += "</ul>";
+        resultDiv.innerHTML = resultado;
+
+        // Aplicar animación de desvanecimiento
+        const listaPeliculas = resultDiv.querySelectorAll("li");
+        let delay = 0;
+        listaPeliculas.forEach((pelicula, index) => {
+            setTimeout(() => {
+                pelicula.style.display = "block";
+                pelicula.style.opacity = "0";
+                pelicula.style.transition = "opacity 0.5s";
+                setTimeout(() => {
+                    pelicula.style.opacity = "1";
+                }, 10);
+            }, delay);
+            delay += 300; // Añade un pequeño retraso para cada elemento de la lista
+        });
     } else {
-        for (var i = 2; i <= numeroIngresado; i += 2) {
-            resultado += i + " ";
-        }
-
-        if (resultado === "") {
-            resultado = "No hay números pares en el rango especificado.";
-        }
+        resultDiv.innerHTML = "<p>No se encontraron películas en ese género.</p>";
     }
+}
 
-    document.getElementById("resultado").innerHTML = resultado;
-});
+// Agregar un manejador de eventos al botón de búsqueda
+searchButton.addEventListener("click", buscarPeliculas);
+
 
