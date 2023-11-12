1886d0597a72ba5cb796c08a97a81f42110e873c
Ramiro franco <francoramiro175@gmail.com>
2023-11-12 16:40:29 -0300

    entrega final

--- js/script.js
+++ js/script.js
@@ -1,100 +1,37 @@
-document.addEventListener("DOMContentLoaded", () =>  {
-    const listaProductos = document.getElementById("lista-productos");
-    const listaCarrito = document.getElementById("lista-carrito");
-    const totalElement = document.getElementById("total");
-    const vaciarCarritoBtn = document.getElementById("vaciar-carrito");
+document.addEventListener("DOMContentLoaded", () => {
+    const comidasContainer = document.getElementById("comidas-container");
 
-    // Productos disponibles
-    const productos = [
-        { id: 1, nombre: "Camiseta Nike", precio: 29.99 },
-        { id: 2, nombre: "Zapatillas Adidas", precio: 79.99 },
-        { id: 3, nombre: "Pantalones Levi's", precio: 49.99 },
-        { id: 4, nombre: "Gorra New Era", precio: 19.99 },
-        { id: 5, nombre: "Reloj Casio", precio: 39.99 },
+    const comidas = [
+        { nombre: "Asado", descripcion: "Carne a la parrilla" },
+        { nombre: "Empanadas", descripcion: "Masa rellena de carne, pollo o verduras" },
+        { nombre: "Milanesa", descripcion: "Carne empanada y frita" },
+        { nombre: "Locro", descripcion: "Guiso de maíz, carne y verduras" },
+        { nombre: "Pastel de papas", descripcion: "Carne molida con pure de papas" },
+        { nombre: "Fideos con tuco", descripcion: "Fideos con base de huevo acompañados de salsa de tomate" },
+        { nombre: "Bifes a la riojana", descripcion: "Carne saltada con verduras" },
+        { nombre: "Arroz con pollo", descripcion: "Arroz salteado con verduas y pata muslo" },
+        { nombre: "Tamal", descripcion: "Masa de maiz rellena de pollo o carne, envuelta en hoja de maiz" },
+        { nombre: "Escalopes", descripcion: "Empanado de harina con carne o pollo frito" },
+        { nombre: "Papas fritas", descripcion: "Papas cortadas finamente, fritas en aceite de girasol" },
+        { nombre: "Pizza", descripcion: "Masa de harina con queso muzzarella y salsa de tomate" },
+        
+        // Agrega más comidas típicas
     ];
 
-    // Event listeners para productos
-    mostrarProductos();
-
-    // Event listener para vaciar carrito
-    vaciarCarritoBtn.addEventListener("click", vaciarCarrito);
-
-    function mostrarProductos() {
-        productos.forEach(producto => {
-            const li = document.createElement("li");
-            li.innerHTML = `${producto.nombre} - $${producto.precio}
-            <input type="number" min="1" value="1">
-            <button class="agregar-carrito" data-id="${producto.id}">Agregar al Carrito</button>`;
-            listaProductos.appendChild(li);
+    const renderComidas = () => {
+        comidas.forEach(comida => {
+            const card = document.createElement('div');
+            card.className = 'card col-md-4 mb-4';
+            card.innerHTML = `
+                <div class="card-body">
+                    <h5 class="card-title">${comida.nombre}</h5>
+                    <p class="card-text">${comida.descripcion}</p>
+                </div>
+            `;
+            comidasContainer.appendChild(card);
         });
-    }
+    };
 
-    // Función para agregar producto al carrito
-    function agregarProducto(producto, cantidad) {
-        const li = document.createElement("li");
-        li.innerHTML = `${producto.nombre} - $${producto.precio} x ${cantidad}`;
-        listaCarrito.appendChild(li);
-
-        // Calcular y actualizar total
-        let total = parseFloat(totalElement.textContent);
-        total += producto.precio * cantidad;
-        totalElement.textContent = total;
-
-        // Guardar carrito en JSON en el almacenamiento local
-        guardarCarritoEnLocalStorage();
-    }
-
-  // ...
-
-// Event listener para productos
-listaProductos.addEventListener("click", e => {
-    if (e.target.classList.contains("agregar-carrito")) {
-        const productoId = parseInt(e.target.getAttribute("data-id"));
-        const producto = productos.find(p => p.id === productoId);
-        const cantidad = parseInt(e.target.previousElementSibling.value);
-        if (producto && cantidad > 0) {
-            agregarProducto(producto, cantidad);
-        }
-    }
-});
-
-// Función para vaciar carrito
-function vaciarCarrito() {
-    while (listaCarrito.firstChild) {
-        listaCarrito.removeChild(listaCarrito.firstChild);
-    }
-    
-    totalElement.textContent = "0";
-
-    // Vaciar carrito en el almacenamiento local
-    localStorage.removeItem("carrito");
-}
-
-// Event listener para el botón de "Vaciar Carrito"
-vaciarCarritoBtn.addEventListener("click", vaciarCarrito);
-
-// Función para guardar el carrito en el almacenamiento local como JSON
-function guardarCarritoEnLocalStorage() {
-    const productosCarrito = Array.from(listaCarrito.children).map(item => item.textContent.trim());
-    localStorage.setItem("carrito", JSON.stringify(productosCarrito));
-}
-
-// Función para cargar carrito desde el almacenamiento local
-function cargarCarritoDesdeLocalStorage() {
-    const carrito = localStorage.getItem("carrito");
-    if (carrito) {
-        const productosCarrito = JSON.parse(carrito);
-        productosCarrito.forEach(item => {
-            const li = document.createElement("li");
-            li.textContent = item;
-            listaCarrito.appendChild(li);
-        });
-    }
-}
-
-// Mostrar productos al cargar la página
-mostrarProductos();
-
-// Cargar carrito desde el almacenamiento local al cargar la página
-cargarCarritoDesdeLocalStorage();
+    // Llama a la función para renderizar las comidas
+    renderComidas();
 });
